be989c715bfce89a1d58d06d0b4d5ddbfe22a21a
Judah Jacobson <judah@users.noreply.github.com>
2017-05-19 17:03:40 -0700

    Allow .proto files to import between Haskell packages.
    Also adds the `proto-lens-protobuf-types` package which includes
    `Proto.Google.Protobuf.{Any,Duration,Wrappers}`.
    
    This change allows one Haskell package to generate bindings for `foo.proto`,
    and another package to generate bindings for `bar.proto`, where
    `bar.proto` imports `foo.proto`.
    
    Under the hood, each package copies its `.proto` files into
    `$datadir/proto-lens-imports`, so that other packages can find
    and use them when compiling their own `.proto` files.
    
    Note: a minor corner case is not handled correctly: if a test or
    benchmark requires va .proto that imports from another package,
    the *library* also needs to specify that dependency.  See
    `proto-lens-tests.cabal` for more details.

---
+++ proto-lens-tests/tests/package-deps_test.hs
@@ -0,0 +1,17 @@
+module Main where
+
+import Data.ProtoLens (def)
+import Lens.Family2 ((&), (.~), (^.))
+import Test.Framework.Providers.HUnit (testCase)
+import Test.HUnit ((@=?))
+
+import Data.ProtoLens.TestUtil (Test, testMain)
+import Proto.PackageDeps (Bar, foo)
+import Proto.TestDep.Foo (value)
+
+main :: IO ()
+main = testMain [testWrapper]
+
+testWrapper :: Test
+testWrapper = testCase "testWrapper" $
+    42 @=? (def & foo . value .~ 42 :: Bar) ^. foo . value
