db9fd9505ab634792c60272cca72b86fcf0c227c
Sandy Maguire <sandy@sandymaguire.me>
2017-11-14 12:03:16 -0700

    need message constraints

--- proto-lens/src/Data/ProtoLens/GRPC/Types.hs
+++ proto-lens/src/Data/ProtoLens/GRPC/Types.hs
@@ -2,16 +2,19 @@
 
 module Data.ProtoLens.GRPC.Types where
 
+import Data.ProtoLens.Message (Message)
+
+
 type ServerHandler       a b = a                                 -> IO b
 type ServerReaderHandler a b =      IO (Maybe a)                 -> IO b
 type ServerWriterHandler a b = a                 -> (b -> IO ()) -> IO ()
 type ServerRWHandler     a b = a -> IO (Maybe a) -> (b -> IO ()) -> IO ()
 
 data SomeHandler where
-  NormalHandler :: ServerHandler a b       -> SomeHandler
-  ReaderHandler :: ServerReaderHandler a b -> SomeHandler
-  WriterHandler :: ServerWriterHandler a b -> SomeHandler
-  BiDiHandler   :: ServerRWHandler a b     -> SomeHandler
+  NormalHandler :: (Message a, Message b) => ServerHandler       a b -> SomeHandler
+  ReaderHandler :: (Message a, Message b) => ServerReaderHandler a b -> SomeHandler
+  WriterHandler :: (Message a, Message b) => ServerWriterHandler a b -> SomeHandler
+  BiDiHandler   :: (Message a, Message b) => ServerRWHandler     a b -> SomeHandler
 
 class Service s where
   packHandlers :: s -> [SomeHandler]
