454af57bac74d787c59f07000e1f55c2087d1d90
Fintan Halpenny <fintan.halpenny@gmail.com>
2017-12-13 11:21:36 +0000

    WIP: trying to replicate RAM increase

---
+++ proto-lens-tests/tests/arbitrary_test.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Main (main) where
+
+import Data.ProtoLens.Arbitrary
+import Test.Hspec
+import Test.QuickCheck
+import Data.ProtoLens (decodeMessage, encodeMessage)
+import Proto.Arbitrary
+
+main :: IO ()
+main = hspec $
+  describe "roundtrip" $ do
+    it "should actually work" $
+     forAllShrink arbitraryMessage shrinkMessage $ \(msg :: Foo) ->
+     decodeMessage (encodeMessage msg) === pure msg
