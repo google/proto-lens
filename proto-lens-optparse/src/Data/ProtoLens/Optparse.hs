f73627ec70d801f4ffb548be201a920056534d4b
Andrew Pritchard <awpritchard@gmail.com>
2018-03-14 19:32:22 -0700

    Add MessageEnum ReadM and option builders. (#181)
    * Add MessageEnum ReadM and option builders.
    
    * Restore support for optparse-applicative <0.13.0.0.

--- proto-lens-optparse/src/Data/ProtoLens/Optparse.hs
+++ proto-lens-optparse/src/Data/ProtoLens/Optparse.hs
@@ -8,12 +8,19 @@
 -- This gives an easy way to define options and arguments for
 -- text-format protobuf types.
 module Data.ProtoLens.Optparse
-    ( proto
+    ( -- * Messages
+      proto
     , protoOption
     , protoArgument
+      -- * Enums
+    , protoEnum
+    , enumOption
+    , enumArgument
     ) where
 
-import Data.ProtoLens.Message (Message)
+import Control.Applicative ((<|>))
+import Control.Monad ((<=<))
+import Data.ProtoLens.Message (Message, MessageEnum(readEnum, maybeToEnum))
 import Data.ProtoLens.TextFormat (readMessage)
 import qualified Data.Text.Lazy as TL
 import Options.Applicative
@@ -26,6 +33,7 @@ import Options.Applicative
   , eitherReader
   , option
   )
+import Text.Read (readMaybe)
 
 -- | An optparse-applicative 'ReadM' for a text-format protobuf.  This lets you
 -- have flags or arguments with protobuf values.
@@ -39,3 +47,20 @@ protoOption = option proto
 -- | Shorthand for a text-format protobuf argument.
 protoArgument :: Message a => Mod ArgumentFields a -> Parser a
 protoArgument = argument proto
+
+-- We define our own maybeReader to preserve compatibility with versions of
+-- optparse-applicative that don't provide it (< 0.13.0.0).
+maybeReader :: (String -> Maybe a) -> ReadM a
+maybeReader = eitherReader . fmap (maybe (Left "No parse") Right)
+
+-- | An optparse-applicative 'ReadM' for an enum name or number.
+protoEnum :: MessageEnum a => ReadM a
+protoEnum = maybeReader readEnum <|> maybeReader (maybeToEnum <=< readMaybe)
+
+-- | Shorthand for a text-format enumbuf option.
+enumOption :: MessageEnum a => Mod OptionFields a -> Parser a
+enumOption = option protoEnum
+
+-- | Shorthand for a text-format enumbuf argument.
+enumArgument :: MessageEnum a => Mod ArgumentFields a -> Parser a
+enumArgument = argument protoEnum
