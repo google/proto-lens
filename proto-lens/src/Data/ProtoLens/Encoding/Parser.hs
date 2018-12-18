049d3b912f9f1253193722b2181d1e7820bc2206
Judah Jacobson <judah@users.noreply.github.com>
2018-12-17 20:29:57 -0800

    CPS the Parser.

--- proto-lens/src/Data/ProtoLens/Encoding/Parser.hs
+++ proto-lens/src/Data/ProtoLens/Encoding/Parser.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE RankNTypes #-}
 module Data.ProtoLens.Encoding.Parser
     ( Parser
     , runParser
@@ -14,7 +15,6 @@ module Data.ProtoLens.Encoding.Parser
 import Foreign.Ptr
 import Foreign.Storable
 import Control.Applicative (Alternative(..))
-import Control.Monad.Trans.Except
 import Data.Word (Word8)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Unsafe as B
@@ -23,45 +23,43 @@ import System.IO.Unsafe
 import Control.Monad (ap)
 import Control.Monad.IO.Class
 
-newtype Parser a = Parser { unParser :: Ptr Word8 -- end
+newtype Parser a = Parser { unParser :: forall r . Ptr Word8 -- end
                                      -> Ptr Word8 -- current position
-                                     -> ExceptT String IO (ParserResult a) }
+                                     -> (String -> r)
+                                     -> (Ptr Word8 -> a -> IO r)
+                                     -> IO r }
 
 -- TODO: remove this
 instance Alternative Parser where
-    empty = Parser $ \_ _ -> fail "empty"
-    f <|> g = Parser $ \end cur ->
-        unParser f end cur <|> unParser g end cur
+    empty = fail "empty"
+    _ <|> _ = undefined {- Parser $ \end cur ->
+        unParser f end cur <|> unParser g end cur -}
 
-data ParserResult a = ParserResult !(Ptr Word8) !a
+-- data ParserResult a = ParserResult !(Ptr Word8) !a
 
 -- todo: kill?
 pFail :: String -> Parser a
 pFail = fail
 
-unParserResult :: ParserResult a -> a
-unParserResult (ParserResult _ x) = x
-
-instance Functor ParserResult where
-    fmap f (ParserResult p x) = ParserResult p (f x)
-
 instance Functor Parser where
-    fmap f (Parser g) = Parser $ \end cur -> fmap (fmap f) $ g end cur
+    fmap f (Parser g) = Parser $ \end cur failure success
+            -> g end cur failure (\cur' x -> success cur' (f x))
 
 instance Applicative Parser where
-    pure x = Parser $ \_ cur -> return $ ParserResult cur x
+    pure x = Parser $ \_ cur _ success -> success cur x
     (<*>) = ap
 
 instance Monad Parser where
-    fail s = Parser $ \_ _ -> throwE s
+    fail s = Parser $ \_ _ failure _-> return $ failure s
     return = pure
-    Parser f >>= g = Parser $ \end cur -> do
-        ParserResult p x <- f end cur
-        unParser (g x) end p
+    Parser f >>= g = Parser $ \end cur failure success ->
+        f end cur failure $ \cur' x -> unParser (g x) end cur' failure success
 
 -- TODO: internal only?  Or do we want this?
 instance MonadIO Parser where
-    liftIO m = Parser $ \_ p -> ParserResult p <$> liftIO m
+    liftIO m = Parser $ \_ cur _ success -> do
+        x <- m
+        success cur x
 
 -- TODO: use ST?
 
@@ -71,16 +69,19 @@ instance MonadIO Parser where
 -- input is discarded and the parser still succeeds.
 runParser :: Parser a -> ByteString -> Either String a
 runParser (Parser m) b = unsafePerformIO $ B.unsafeUseAsCStringLen b $ \(p, len)
-    -> runExceptT $ fmap unParserResult $ m (p `plusPtr` len) (castPtr p)
+    -> m (p `plusPtr` len) (castPtr p) Left $ \_ -> return . Right
 
+-- TODO: names
+-- gets the current
 get :: Parser (Ptr Word8)
-get = Parser $ \_ p -> return $ ParserResult p p
+get = Parser $ \_ p _ success -> success p p
 
+-- Gets the end
 ask :: Parser (Ptr Word8)
-ask = Parser $ \end p -> return $ ParserResult p end
+ask = Parser $ \end p _ success -> success p end
 
 put :: Ptr Word8 -> Parser ()
-put p = Parser $ \_ _ -> return $ ParserResult p ()
+put !p = Parser $ \_ _ _ success -> success p ()
 
 atEnd :: Parser Bool
 atEnd = do
@@ -119,4 +120,6 @@ isolate n (Parser m) = do
 
 -- TODO: I'm not convinced this works...
 (<?>) :: Parser a -> String -> Parser a
-Parser m <?> msg = Parser $ \end p -> withExceptT (\s -> msg ++ ": " ++ s) $ m end p
+Parser m <?> msg = Parser $ \end p failure success ->
+    -- TODO: the concat is probably inefficient
+    m end p (\s -> failure $ s ++ ":" ++ msg) success
