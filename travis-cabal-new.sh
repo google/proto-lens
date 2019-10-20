3cee08eaac00d5738bec0e5578fe1330ed9a83c0
Judah Jacobson <judah@users.noreply.github.com>
2019-10-20 08:39:47 -0700

    Support cabal new-build, and add a Travis case.
    - Make all the .cabal files explicitly declare
      `build-tool-depends: proto-lens-protoc:proto-lens-protoc`.
      Also bump hpack to support that syntax.
    - Use `generated-exposed-modules` and `generated-other-modules`
      consistently.
    
    For now, Travis just has a single case (8.6.5).  Switching
    completely is be blocked on #354 (including .protos from deps).

---
+++ travis-cabal-new.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# A script for running Cabal new-build on this project.
+
+set -euo pipefail
+set -x
+
+echo Installing hpack
+curl -L https://github.com/sol/hpack/releases/download/0.31.2/hpack_linux.gz \
+  | gunzip > $HOME/.local/bin/hpack
+chmod +x $HOME/.local/bin/hpack
+
+# Work around #354: With new-build we don't include .proto files from
+# dependencies in the same project.
+# For now, just copy the files in directly.
+cp -Rv proto-lens-tests-dep/protos/* proto-lens-tests/tests/
+
+for p in */package.yaml
+do
+  hpack $p
+done
+
+cabal new-update
+cabal new-test all
