9a21cf725db2a99c70d76bf03db1eae3ce3c6afb
Judah Jacobson <judah@users.noreply.github.com>
2019-10-25 21:00:23 -0700

    Move descriptor/plugin bindings to proto-lens-protobuf-types. (#358)
    Previously the checked-in bootstrap protos were part of the proto-lens package.
    After this change, they're included in the `other-modules` of
    proto-lens-protoc`.  Instead, the exposed versions of the modules are
    provided by proto-lens-protobuf-types, just like the other proto
    files.
    
    Fixed #344, which describes why this was causing issues with some
    build systems like Bazel.
    
    Another side effect of this change is: previously the bootstrap protos
    had special logic for how they imported modules.  After this change
    they can depend on them from proto-lens-runtime, just like other
    generated protos.  This simplifies some logic in proto-lens-protoc.
    
    Also update the bootstrap script.  It's mostly the same as before,
    except that we first checkout an old version of the bootstrap proto
    bindings that will be compatible with the version of proto-lens we're
    bootstrapping against. (They'll be overwritten at the end of the
    script anyway.)

--- bootstrap.hs
+++ bootstrap.hs
@@ -15,24 +15,38 @@ import System.Process (callProcess, readProcess)
 
 protoRoot = "google/protobuf/src"
 protoc = "protoc"
-bootstrapModuleRoot = "proto-lens/src"
+bootstrapModuleRoot = "proto-lens-protoc/app"
 useBootstrappingYaml = "--stack-yaml=stack-bootstrap.yaml"
 
+-- Change this to build with an older version of stack.
+-- TODO: remove this after we can use stack v2 (#332).
+stack = "stack"
+
+-- This should match (or at least be API-compatible with) the value of bootstrapCommit
+-- in stack-bootstrap.yaml.
+bootstrapCommit = "master"
+
 main = do
-  callProcess "stack" [useBootstrappingYaml, "setup"]
-  do
-      [installRoot] <- lines <$> readProcess "stack"
-                        [useBootstrappingYaml, "path", "--local-install-root"] ""
-      let protocGenHaskell = installRoot </> "bin/proto-lens-protoc"
-      callProcess "stack" [useBootstrappingYaml, "build", "proto-lens-protoc"]
-      callProcess protoc $
-          [ "--plugin=protoc-gen-haskell=" ++ protocGenHaskell
-          , "--haskell_out=no-runtime:" ++ bootstrapModuleRoot
-          , "--proto_path=" ++ protoRoot
-          ]
-          ++ map (protoRoot </>)
-              [ "google/protobuf/descriptor.proto"
-              , "google/protobuf/compiler/plugin.proto"
-              ]
-      -- Verify that the generated code compiles successfully.
-      callProcess "stack" ["build", "proto-lens"]
+    -- 1. Temporarily replace the bootstrap proto bindings in proto-lens-protoc
+    --    with an older version that's compatible with the bootstrap version of
+    --    proto-lens.
+    -- 2. Build proto-lens-protoc against the older proto-lens.
+    -- 3. Use it to generate new versions of the bootstrap proto bindings,
+    --    overwriting the previous versions.
+    callProcess "git" ["checkout", bootstrapCommit, "--",
+                       bootstrapModuleRoot </> "Proto"]
+    [installRoot] <- lines <$> readProcess stack
+                    [useBootstrappingYaml, "path", "--local-install-root"] ""
+    let protocGenHaskell = installRoot </> "bin/proto-lens-protoc"
+    callProcess stack [useBootstrappingYaml, "build", "proto-lens-protoc"]
+    callProcess protoc $
+        [ "--plugin=protoc-gen-haskell=" ++ protocGenHaskell
+        , "--haskell_out=no-runtime:" ++ bootstrapModuleRoot
+        , "--proto_path=" ++ protoRoot
+        ]
+        ++ map (protoRoot </>)
+            [ "google/protobuf/descriptor.proto"
+            , "google/protobuf/compiler/plugin.proto"
+            ]
+    -- Verify that the generated code compiles successfully.
+    callProcess stack ["build", "proto-lens-protoc"]
