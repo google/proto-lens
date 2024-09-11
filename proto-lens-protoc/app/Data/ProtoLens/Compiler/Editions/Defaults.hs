9320b6a0ab739889d77370065b4429ad252bb906
Yoo Chung <chungyc@google.com>
2024-09-11 03:21:04 +0000

    Restrict supported editions to real editions.

--- proto-lens-protoc/app/Data/ProtoLens/Compiler/Editions/Defaults.hs
+++ proto-lens-protoc/app/Data/ProtoLens/Compiler/Editions/Defaults.hs
@@ -35,11 +35,11 @@ This contains the defaults from editions @LEGACY@ to @EDITION_2023@
 for the native features defined by @google.protobuf.FeatureSet@.
 The message was generated with @protoc@ and translated into a Haskell string:
 
-> $ protoc --edition_defaults_out=defaults.binpb --edition_defaults_minimum=LEGACY --edition_defaults_maximum=2023 google/protobuf/descriptor.proto
+> $ protoc --edition_defaults_out=defaults.binpb --edition_defaults_minimum=PROTO2 --edition_defaults_maximum=2023 google/protobuf/descriptor.proto
 > $ ghci
 > ghci> import Data.ByteString as B
 > ghci> B.readFile "defaults.binpb" >>= print . show
 
 -}
 serializedNativeDefaults :: ByteString
-serializedNativeDefaults = read "\"\\n\\DC3\\CAN\\132\\a\\\"\\NUL*\\f\\b\\SOH\\DLE\\STX\\CAN\\STX \\ETX(\\SOH0\\STX\\n\\DC3\\CAN\\231\\a\\\"\\NUL*\\f\\b\\STX\\DLE\\SOH\\CAN\\SOH \\STX(\\SOH0\\SOH\\n\\DC3\\CAN\\232\\a\\\"\\f\\b\\SOH\\DLE\\SOH\\CAN\\SOH \\STX(\\SOH0\\SOH*\\NUL \\132\\a(\\232\\a\""
+serializedNativeDefaults = read "\"\\n\\DC3\\CAN\\132\\a\\\"\\NUL*\\f\\b\\SOH\\DLE\\STX\\CAN\\STX \\ETX(\\SOH0\\STX\\n\\DC3\\CAN\\231\\a\\\"\\NUL*\\f\\b\\STX\\DLE\\SOH\\CAN\\SOH \\STX(\\SOH0\\SOH\\n\\DC3\\CAN\\232\\a\\\"\\f\\b\\SOH\\DLE\\SOH\\CAN\\SOH \\STX(\\SOH0\\SOH*\\NUL \\230\\a(\\232\\a\""
